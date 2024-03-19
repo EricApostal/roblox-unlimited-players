b3c2da515eeeb9b715af4c8af7c639d6e35aa85f
EricApostal <60072374+EricApostal@users.noreply.github.com>
2024-03-19 18:02:48 -0400

    rotating subscription, still kinda buggy

--- src/server/services/networking.ts
+++ src/server/services/networking.ts
@@ -6,24 +6,59 @@ let serverId = HttpService.GenerateGUID(false)
 
 class Topic {
     id: string;
+    listenConnection: RBXScriptConnection | undefined;
+    private currentDisposeRequests: number = 0;
 
     constructor(id: string) {
         this.id = id
     }
 
-    listen() {
-        MessagingService.SubscribeAsync(this.id, (_d: unknown) => {
-            let data = (_d as Map<string, unknown>).get("Data");
+    private listenAsync() {
+        return MessagingService.SubscribeAsync(this.id, (_d: unknown) => {
+            let data = (_d as Map<string, unknown>).get("Data") as Map<string, unknown>;
+            let newId = data.get("nextId") as string;
 
-            if (data === serverId) return;
-            print(`Received data: ${data} from ${this.id}`)
+            print(`Received request from ${this.id}`)
+        
+            if (this.id === newId) return;
+
+            // if we are rotating the ID
+            print("Rotating ID!")
+            this.id = newId;
+            
+            let old = this.listenConnection;
+            this.listenConnection = this.listenAsync();
+            old?.Disconnect();
+
+            
+            
         })
     }
 
+    listen() {
+        this.listenConnection = this.listenAsync();
+    }
+
     send(data: unknown) {
+        this.currentDisposeRequests += 1;
+
+        let sendData = new Map<string, unknown>()
+        let newId = this.id;
+
+        if (this.currentDisposeRequests >= 2) {
+            newId = HttpService.GenerateGUID(false);
+            this.currentDisposeRequests = 0;
+        }
+
+        sendData.set("id", serverId);
+        sendData.set("nextId", newId)
+        sendData.set("data", data);
+
         let status = pcall(() => {
-            MessagingService.PublishAsync(this.id, data);
-        });        
+            MessagingService.PublishAsync(this.id, sendData);
+            this.id = newId;
+        });
+        
         if (!status[0]) {
             print(`Failed to send data!`)
             warn(status[1])
@@ -33,21 +68,22 @@ class Topic {
 
 @Service()
 export class NetworkService extends BaseComponent implements OnStart {
-    threadCount: number = 20;
+    threadCount: number = 10;
     onStart(): void {
         print("Started the messaging service!")
 
         let topics = new Map<string, Topic>()
         for (let i = 0; i < this.threadCount; i++) {
             let topic = new Topic(string.format("topic-%d", i))
+            // print(`Created topic ${topic.id}`)
             topics.set(topic.id, topic)
             topic.listen()
         }
 
         while (true) {
             for (let [_, topic] of topics) {
-                topic.send(serverId);
-                wait(0.34);
+                topic.send({});
+                wait(0.25);
             }
         }
     }

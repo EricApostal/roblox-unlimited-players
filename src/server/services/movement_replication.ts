16d380b2a5242f15ab42f722a6f0a4d6e6a97f5e
EricApostal <ehapostal04@gmail.com>
2024-03-20 09:31:44 -0400

    added movement replication

--- src/server/services/movement_replication.ts
+++ src/server/services/movement_replication.ts
@@ -1,16 +1,18 @@
 import { OnStart, Service } from "@flamework/core";
 import { Event, EventType, NetworkService } from "./networking";
 import { BaseComponent } from "@flamework/components";
+import { Players } from "@rbxts/services";
 
 @Service()
 export class TestService extends BaseComponent implements OnStart {
     onStart(): void {
-        print("running test service");
         while (true) {
-            NetworkService.queueEvent(new Event({
-                player: "test",
-                position: { x: 0, y: 0, z: 0 }
-            }, EventType.PlayerPositionUpdate));
+            for (const player of Players.GetPlayers()) {
+                NetworkService.queueEvent(new Event({
+                    playerId: player.UserId,
+                    position: { x: 0, y: 0, z: 0 }
+                }, EventType.PlayerPositionUpdate));
+            }
             wait();
         }
     }

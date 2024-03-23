1783522862b73066296f25a024126fc83aa9fe42
Eric Apostal <ehapostal04@gmail.com>
2024-03-23 13:10:26 -0400

    migrate out of animation service

--- src/server/services/network/replication/animation.ts
+++ src/server/services/network/replication/animation.ts
@@ -1,69 +1,71 @@
-import { BaseComponent } from "@flamework/components";
-import { OnStart, Service } from "@flamework/core";
-import { Event, EventType, NetworkService, ServerRequest } from "server/services/networking";
-import { Players, HttpService, ReplicatedStorage } from "@rbxts/services";
-import { OnServerRequestRecieved } from "../bindings";
-import { replicationRigs } from "./state";
+// import { BaseComponent } from "@flamework/components";
+// import { OnStart, Service } from "@flamework/core";
+// import { Event, EventType, NetworkService, ServerRequest } from "server/services/networking";
+// import { Players, HttpService, ReplicatedStorage } from "@rbxts/services";
+// import { OnServerRequestRecieved } from "../bindings";
+// import { replicationRigs } from "./state";
 
-enum AnimationType {
-    Running,
-    Jumping,
-    Idle
-}
+// enum AnimationType {
+//     Running,
+//     Jumping,
+//     Idle
+// }
 
-@Service()
-export class PlayerAnimationReplicationService extends BaseComponent implements OnStart, OnServerRequestRecieved {
-    currentAnimationTrack: AnimationTrack | undefined;
+// @Service()
+// export class PlayerAnimationReplicationService extends BaseComponent implements OnStart, OnServerRequestRecieved {
+//     currentAnimationTrack: AnimationTrack | undefined;
 
-    onStart(): void {
-        Players.PlayerAdded.Connect((player) => {
-            player.CharacterAdded.Connect((char) => {
-                let humanoid = char.WaitForChild("Humanoid") as Humanoid;
-                // on humanoid run
-                humanoid.Running.Connect((speed: number) => {
-                    if (speed > 0) {
-                        NetworkService.queueEvent(new Event(AnimationType.Running, EventType.PlayerAnimationUpdate));
-                    } else {
-                        NetworkService.queueEvent(new Event(AnimationType.Idle, EventType.PlayerAnimationUpdate));
-                    }
-                });
+//     onStart(): void {
+//         Players.PlayerAdded.Connect((player) => {
+//             player.CharacterAdded.Connect((char) => {
+//                 let humanoid = char.WaitForChild("Humanoid") as Humanoid;
+//                 // on humanoid run
+//                 humanoid.Running.Connect((speed: number) => {
+//                     if (speed > 0) {
+//                         NetworkService.queueEvent(new Event(AnimationType.Running, EventType.PlayerAnimationUpdate));
+//                     } else {
+//                         NetworkService.queueEvent(new Event(AnimationType.Idle, EventType.PlayerAnimationUpdate));
+//                     }
+//                 });
 
-                humanoid.Jumping.Connect(() => {
-                    NetworkService.queueEvent(new Event(AnimationType.Jumping, EventType.PlayerAnimationUpdate));
-                });
+//                 humanoid.Jumping.Connect(() => {
+//                     NetworkService.queueEvent(new Event(AnimationType.Jumping, EventType.PlayerAnimationUpdate));
+//                 });
 
-            });
-        });
-    }
+//             });
+//         });
+//     }
 
-    onServerRequestRecieved(request: ServerRequest): void {
-        let playerId = request.id;
+//     onServerRequestRecieved(request: ServerRequest): void {
+//         let playerId = request.id;
 
-        for (let event of request.events as Array<Event>) {
-            if (event.eT === EventType.PlayerAnimationUpdate) {
-                let data = event.d as AnimationType;
-                if (replicationRigs.get(playerId)) {
-                    let humanoid = replicationRigs.get(playerId)!.WaitForChild("Humanoid") as Humanoid;
-                    if (data === AnimationType.Running) {
-                        let animation = new Instance("Animation") as Animation;
-                        animation.AnimationId = "rbxassetid://507767714";
-                        this.currentAnimationTrack = (humanoid.WaitForChild("Animator") as Animator).LoadAnimation(animation);
-                        this.currentAnimationTrack.Play();
-                    }
+//         for (let event of request.events as Array<Event>) {
+//             if (event.eT === EventType.PlayerAnimationUpdate) {
+//                 let data = event.d as AnimationType;
+//                 if (replicationRigs.get(playerId)) {
+//                     let humanoid = replicationRigs.get(playerId)!.WaitForChild("Humanoid") as Humanoid;
+//                     if (data === AnimationType.Running) {
+//                         print("running so playing")
+//                         let animation = new Instance("Animation") as Animation;
+//                         animation.AnimationId = "rbxassetid://507767714";
+//                         this.currentAnimationTrack = (humanoid.WaitForChild("Animator") as Animator).LoadAnimation(animation);
+//                         this.currentAnimationTrack.Play();
+//                     }
 
-                    if (data === AnimationType.Jumping) {
-                        let animation = new Instance("Animation") as Animation;
-                        animation.AnimationId = "rbxassetid://507765000";
-                        this.currentAnimationTrack = (humanoid.WaitForChild("Animator") as Animator).LoadAnimation(animation);
-                        this.currentAnimationTrack.Play();
-                    }
+//                     if (data === AnimationType.Jumping) {
+//                         let animation = new Instance("Animation") as Animation;
+//                         animation.AnimationId = "rbxassetid://507765000";
+//                         this.currentAnimationTrack = (humanoid.WaitForChild("Animator") as Animator).LoadAnimation(animation);
+//                         this.currentAnimationTrack.Play();
+//                     }
 
-                    if (data === AnimationType.Idle) {
-                        this.currentAnimationTrack?.Stop();
-                        this.currentAnimationTrack?.Destroy();
-                    }
-                }
-            }
-        }
-    }
-}
+//                     if (data === AnimationType.Idle) {
+//                         print("idle so stopping")
+//                         this.currentAnimationTrack?.Stop();
+//                         this.currentAnimationTrack?.Destroy();
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
--- src/server/services/network/replication/movement.ts
+++ src/server/services/network/replication/movement.ts
@@ -5,8 +5,18 @@ import { Players, HttpService, ReplicatedStorage } from "@rbxts/services";
 import { OnServerRequestRecieved } from "../bindings";
 import { replicationRigs } from "./state";
 
+enum AnimationType {
+    Running,
+    Jumping,
+    Idle
+}
+
 @Service()
 export class PlayerMovementReplicationService extends BaseComponent implements OnServerRequestRecieved, OnStart {
+    lastAnimationType: AnimationType | undefined;
+    currentAnimationTrack: AnimationTrack | undefined;
+    isJumping: boolean = false;
+
     onStart() {
         while (true) {
             for (const player of Players.GetPlayers()) {
@@ -18,9 +28,24 @@ export class PlayerMovementReplicationService extends BaseComponent implements O
                 let pos = (player!.Character!.PrimaryPart!.Position);
                 let precision = 100;
 
+                let speed = (player.Character!.PrimaryPart!.Velocity.Magnitude);
+                let isJumping = (player.Character!.FindFirstChild("Humanoid") as Humanoid)!.Jump
+
+                let animationType = AnimationType.Idle;
+                if (speed > 0) {
+                    animationType = AnimationType.Running;
+                }
+
+                if (isJumping) {
+                    animationType = AnimationType.Jumping;
+                }
+
+
+
                 NetworkService.queueEvent(new Event([
                     { x: math.round(pos.X * precision) / precision, y: math.round(pos.Y * precision) / precision, z: math.round(pos.Z * precision) / precision },
-                    { x: math.round(char.PrimaryPart!.Orientation.X * precision) / precision, y: math.round(char.PrimaryPart!.Orientation.Y * precision) / precision, z: math.round(char.PrimaryPart!.Orientation.Z * precision) / precision }
+                    { x: math.round(char.PrimaryPart!.Orientation.X * precision) / precision, y: math.round(char.PrimaryPart!.Orientation.Y * precision) / precision, z: math.round(char.PrimaryPart!.Orientation.Z * precision) / precision },
+                    animationType
                 ], EventType.PlayerPositionUpdate));
             }
             wait();
@@ -32,11 +57,12 @@ export class PlayerMovementReplicationService extends BaseComponent implements O
 
         for (let event of request.events as Array<Event>) {
             if (event.eT === EventType.PlayerPositionUpdate) {
-                let data = event.d as [{ x: number, y: number, z: number }, r: { x: number, y: number, z: number }];
+                let data = event.d as [{ x: number, y: number, z: number }, r: { x: number, y: number, z: number }, AnimationType];
 
                 let playerReplicated = replicationRigs.get(playerId as number);
                 let position = data[0];
                 let orientation = data[1];
+                let animationType = data[2] as AnimationType;
 
                 if (!playerReplicated) {
                     playerReplicated = ReplicatedStorage.WaitForChild("assets").WaitForChild("Rig").Clone() as Model;
@@ -54,6 +80,41 @@ export class PlayerMovementReplicationService extends BaseComponent implements O
 
                 if ((playerReplicated.PrimaryPart!.Orientation.sub(new Vector3(0, orientation.y, 0))).Magnitude >= 0.5) {
                     goal.set("Orientation", new Vector3(0, orientation.y, 0));
+                }
+
+                // do animation
+                if ((this.lastAnimationType !== animationType) && !this.isJumping) {
+                    if (animationType === AnimationType.Running) {
+                        this.lastAnimationType = AnimationType.Running;
+                        let animation = new Instance("Animation") as Animation;
+                        animation.AnimationId = "rbxassetid://507767714";
+                        this.currentAnimationTrack = (playerReplicated.WaitForChild("Humanoid").WaitForChild("Animator") as Animator).LoadAnimation(animation);
+                        this.currentAnimationTrack.Play();
+                    }
+
+
+                    if (animationType === AnimationType.Idle) {
+                        this.lastAnimationType = AnimationType.Idle;
+                        if (this.currentAnimationTrack) {
+                            this.currentAnimationTrack.Stop();
+                            this.currentAnimationTrack.Destroy();
+                        }
+                    }
+
+                    if (animationType === AnimationType.Jumping) {
+                        print("playing jump!")
+                        this.isJumping = true;
+                        this.lastAnimationType = AnimationType.Jumping;
+                        let animation = new Instance("Animation") as Animation;
+                        animation.AnimationId = "rbxassetid://507765000";
+                        this.currentAnimationTrack = (playerReplicated.WaitForChild("Humanoid").WaitForChild("Animator") as Animator).LoadAnimation(animation);
+                        this.currentAnimationTrack.Play();
+                        let _link: RBXScriptConnection | undefined;
+                        _link = this.currentAnimationTrack.Stopped.Connect(() => {
+                            this.isJumping = false;
+                            if (_link) _link.Disconnect();
+                        })
+                    }
                 }
 
                 let tween = game.GetService("TweenService").Create(playerReplicated!.PrimaryPart as BasePart, new TweenInfo(0.2, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut), goal as {});
